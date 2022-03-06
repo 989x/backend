0c9dd97ba679b09aba866b45c2b610ac06c1c302
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-03-06 20:06:35 +0700

    React.js how to fetch data from backend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,18 +3,18 @@ import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts'
 import Sidebar from '../../components/sidebar/Sidebar'
 import "./home.css"
-// import axios from "axios"
+import axios from "axios"
 
 export default function Home() {
-    // const [posts, setPosts] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-    // useEffect(() => {
-    //     const fetchPosts = async () => {
-    //         const res = await axios.get("/")
-    //         console.log(res)
-    //     }
-    //     fetchPosts()
-    // })
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("/posts")
+            console.log(res)
+        }
+        fetchPosts()
+    },[])
     return (
         <>
         <Header/>
