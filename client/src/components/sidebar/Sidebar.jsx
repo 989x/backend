715b73cfcbdddc9c3be0123c776394a03345d269
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-03-07 16:19:36 +0700

    Fetching all categories

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import "./sidebar.css"
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function Sidebar() {
+    const [cats, setCats] = useState([]);
+
+    useEffect(() => {
+        const getCats = async () => {
+            const res = await axios.get("/categories")
+            setCats(res.data)
+        };
+        getCats();
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebarItem">
@@ -19,12 +31,11 @@ export default function Sidebar() {
                 <span className="sidebarTitle">CATEGORIES</span>
                 <ul className="sidebarList">
                     <li className="sidebarListItem">
-                        <li className="sidebarListItem">Life</li>
-                        <li className="sidebarListItem">Music</li>
-                        <li className="sidebarListItem">Style</li>
-                        <li className="sidebarListItem">Sport</li>
-                        <li className="sidebarListItem">Tech</li>
-                        <li className="sidebarListItem">Cinema</li>
+
+                        {cats.map((c) => (
+                            <li className="sidebarListItem">{c.name}</li>
+                        ))}
+
                     </li>
                 </ul>
             </div>
