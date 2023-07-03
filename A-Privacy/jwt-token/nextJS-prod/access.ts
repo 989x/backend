d8923c30330e51a7b6396fdf714b85964c1222d0
SinsamutQ <99growsup@gmail.com>
2023-07-03 21:50:24 +0700

    doing,jwt-token

---
+++ A-Privacy/jwt-token/nextJS-prod/access.ts
@@ -0,0 +1,103 @@
+// https://github.dev/pankornch/tront-cloud-client/blob/dev/pages/api/auth/%5B...nextauth%5D.ts
+
+import client from "@/src/configs/apollo"
+import { OAUTH_MUTATION, SIGN_IN_MUTATION } from "@/src/gql"
+import NextAuth from "next-auth/next" 
+import Credentials from "next-auth/providers/credentials"
+import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+export default NextAuth({
+	providers: [
+	 	Credentials({
+			name: "Credentials",
+			credentials: {
+				email: { label: "email", type: "texact", placeholder: "jsmith" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				try {
+					const { data, errors } = await client.mutate({
+						mutation: SIGN_IN_MUTATION,
+						variables: {
+							input: {
+								email: credentials?.email,
+								password: credentials?.password,
+							},
+						},
+					})
+
+					if (errors) return null
+					const user = data.signIn.user
+					user.token = data.signIn.token
+					return user
+				} catch (error) {
+					console.error(error)
+					return null
+				}
+			},
+		}),
+		GitHubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+		GoogleProvider({
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		}),
+	],
+	pages: {
+		signIn: "/login",
+		signOut: "/signout",
+	},
+	jwt: {
+		secret: process.env.JWT_SECRET,
+	},
+	secret: process.env.SECRET,
+	callbacks: {
+		async signIn({ user, account }) {
+			if (account.type === "credentials") {
+				account.trontAccessToken = user.token
+				account.user = user
+			} else {
+				const { data, errors } = await client.mutate({
+					mutation: OAUTH_MUTATION,
+					variables: {
+						input: {
+							providerAccountId: account.providerAccountId,
+							type: account.provider.toUpperCase(),
+							email: user.email,
+							avatar: user.avatar || user.image,
+						},
+					},
+				})
+				if (errors) {
+					console.error(errors)
+					return false
+				}
+				account.trontAccessToken = data.oauth.token
+				account.user = data.oauth.user
+			}
+			return true
+		},
+		async jwt({ token, account }) {
+			if (account) {
+				token.accessToken = account.access_token
+				token.trontAccessToken = account.trontAccessToken
+				token.user = account.user
+			}
+
+			return token
+		},
+		async session({ session, token }) {
+			if (token) {
+				session.accessToken = token.accessToken
+				session.trontAccessToken = token.trontAccessToken as string
+				// session.user._id = token.uid as string
+				// @ts-ignore
+				session.user = token.user
+			}
+
+			return session
+		},
+	},
+})
--- A-Privacy/jwt-token/readme.md
+++ A-Privacy/jwt-token/readme.md
@@ -1,10 +1,3 @@
-### Reff
-
-สร้างระบบ API Authentication ด้วย JWT Refresh Token กับ NodeJs กัน
-- https://www.borntodev.com/2022/12/15/%E0%B8%AA%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%87%E0%B8%A3%E0%B8%B0%E0%B8%9A%E0%B8%9A-api-authentication-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-jwt-refresh-token-%E0%B8%81%E0%B8%B1%E0%B8%9A-nodejs/
-
----
-
 ### nextJS worked
 
 Next.js Authentication - JWT Refresh Token Rotation with NextAuth.js
@@ -14,8 +7,6 @@ Next.js Authentication - JWT Refresh Token Rotation with NextAuth.js
 
 Refresh Token with next-auth and Axios Interceptors in Next.js 13 Authentication
 - https://www.youtube.com/watch?v=RPl0r-Yl6pU&t=899s
-
----
 
 ### express example
 
